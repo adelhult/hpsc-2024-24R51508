9f7077fa990755a5f0f7c3d7cb18bac75488e9f9
Eli Adelhult <hello@eli.nu>
2024-04-18 21:54:16 +0900

    assignment 2: MPI (work in progress)

--- 03_mpi/11_distributed.cpp
+++ 03_mpi/11_distributed.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
 #include <cmath>
 #include <mpi.h>
 
@@ -13,22 +14,32 @@ int main(int argc, char** argv) {
   int size, rank;
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  Body ibody[N/size], jbody[N/size];
+  const auto array_size = N / size;
+  Body ibody[array_size], jbody[array_size], buffer[array_size];
   srand48(rank);
-  for(int i=0; i<N/size; i++) {
+  for(int i=0; i<array_size; i++) {
     ibody[i].x = jbody[i].x = drand48();
     ibody[i].y = jbody[i].y = drand48();
     ibody[i].m = jbody[i].m = drand48();
     ibody[i].fx = jbody[i].fx = ibody[i].fy = jbody[i].fy = 0;
   }
-  int recv_from = (rank + 1) % size;
   int send_to = (rank - 1 + size) % size;
   MPI_Datatype MPI_BODY;
   MPI_Type_contiguous(5, MPI_DOUBLE, &MPI_BODY);
   MPI_Type_commit(&MPI_BODY);
+  
+  MPI_Win window;
+  MPI_Win_create(buffer, array_size * sizeof(Body), sizeof(Body), MPI_INFO_NULL, MPI_COMM_WORLD, &window);
+  
   for(int irank=0; irank<size; irank++) {
-    MPI_Send(jbody, N/size, MPI_BODY, send_to, 0, MPI_COMM_WORLD);
-    MPI_Recv(jbody, N/size, MPI_BODY, recv_from, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    MPI_Win_fence(0, window);
+    MPI_Put(jbody, array_size, MPI_BODY, send_to, 0, array_size, MPI_BODY, window);
+    MPI_Win_fence(0, window);
+    // Note: I'm not entirely sure if a seperate send buffer actually is needed
+    // (but I want to be careful to avoid jbody being partially overwritten by 
+    // another process at the same time as MPI_Put is running).  
+    std::memcpy(jbody, buffer, array_size * sizeof(Body));
+    
     for(int i=0; i<N/size; i++) {
       for(int j=0; j<N/size; j++) {
         double rx = ibody[i].x - jbody[j].x;
@@ -41,8 +52,11 @@ int main(int argc, char** argv) {
       }
     }
   }
+
+  MPI_Win_free(&window);
+
   for(int irank=0; irank<size; irank++) {
-    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Barrier(MPI_COMM_WORLD); // TODO: Could prob remove this
     if(irank==rank) {
       for(int i=0; i<N/size; i++) {
         printf("%d %g %g\n",i+rank*N/size,ibody[i].fx,ibody[i].fy);
