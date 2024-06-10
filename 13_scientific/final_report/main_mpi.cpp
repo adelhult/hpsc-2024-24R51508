7348a85bfe9caf476d29bc404d002ed774115777
Eli Adelhult <hello@eli.nu>
2024-06-10 20:13:19 +0900

    move comment

--- 13_scientific/final_report/main_mpi.cpp
+++ 13_scientific/final_report/main_mpi.cpp
@@ -70,13 +70,7 @@ int main(int argc, char **argv) {
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-    auto u = Matrix<float>(local_ny + 2, nx, 0.0);
-    auto v = Matrix<float>(local_ny + 2, nx, 0.0);
-    auto p = Matrix<float>(local_ny + 2, nx, 0.0);
-    auto b = Matrix<float>(local_ny + 2, nx, 0.0);
-/*
-
+    
     //const auto nx = 41;
     //const auto ny = 41;
     const auto nx = 10;
@@ -105,7 +99,7 @@ int main(int argc, char **argv) {
     auto v = Matrix<float>(local_ny + 2, nx, 0.0);
     auto p = Matrix<float>(local_ny + 2, nx, 0.0);
     auto b = Matrix<float>(local_ny + 2, nx, 0.0);
-
+/*
     int first_row = 1;
     // The first process has no use for the ghost row at the top
     if (rank == 0) {
