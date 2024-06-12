b10b4389a28195968ebc41a0c0bcf5a3bc67b04f
Eli Adelhult <hello@eli.nu>
2024-06-12 14:42:22 +0900

    testing

--- 13_scientific/10_cavity.py
+++ 13_scientific/10_cavity.py
@@ -86,6 +86,7 @@ for n in range(nt):
     # Debugging
     if (n == 5):
         error_margin = 1e-3 # rounding differences? (At least I hopes so :sweat: !)
+        print((read_file("./final_report/u.txt") - u) < error_margin)
         assert ((read_file("./final_report/u.txt") - u) < error_margin).all()
         assert ((read_file("./final_report/v.txt") -v) < error_margin).all()
         assert ((read_file("./final_report/p.txt") -p) < error_margin).all()
