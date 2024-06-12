ed1120090662c049a6521f0c8d4394fd6437a9a3
Eli Adelhult <hello@eli.nu>
2024-06-12 15:45:05 +0900

    back to 1st iteration

--- 13_scientific/final_report/main_mpi.cpp
+++ 13_scientific/final_report/main_mpi.cpp
@@ -287,7 +287,7 @@ int main(int argc, char **argv) {
 
         // Debugging
 #ifdef DEBUGGING
-        if (n == 5) {
+        if (n == 1) {
             // TODO: this breaks if local_ny is different between processes
             // i.e. when ny is not divisible by size. 
             MPI_Gather(u.get() + nx, nx * local_ny, MPI_FLOAT, u_full->get() + nx * local_ny * rank, nx * local_ny,
